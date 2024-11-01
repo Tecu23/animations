76e292bf4a515d1cc4e6012c18587836f80ffc5b
Tecu23 <andrei.teculescu@gmail.com>
2024-11-01 19:38:21 +0200

    add small menu button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import DraggableCarousel from "./components/carousels/DraggableCarousel";
+import HamburgerMenu from "./components/buttons/HamburgerMenu";
 
 function App() {
     return (
         <>
-            <DraggableCarousel />
+            <div className="grid h-screen place-content-center bg-gradient-to-br from-violet-500 to-indigo-500">
+                <HamburgerMenu />
+            </div>
         </>
     );
 }
---
+++ src/components/buttons/HamburgerMenu.tsx
@@ -0,0 +1,79 @@
+import { motion, MotionConfig } from "framer-motion";
+import { useState } from "react";
+
+const HamburgerMenu = () => {
+    const [active, setActive] = useState(false);
+
+    return (
+        <MotionConfig
+            transition={{
+                duration: 0.5,
+                ease: "easeInOut",
+            }}
+        >
+            <motion.button
+                onClick={() => setActive((prev) => !prev)}
+                className="relative h-20 w-20 rounded-full transition-colors bg-white/0 hover:bg-white/20"
+                animate={active ? "open" : "close"}
+            >
+                <motion.span
+                    style={{
+                        left: "50%",
+                        top: "35%",
+                        x: "-50%",
+                        y: "-50%",
+                    }}
+                    className="absolute h-1 w-10 bg-white"
+                    variants={{
+                        open: {
+                            rotate: ["0deg", "0deg", "45deg"],
+                            top: ["35%", "50%", "50%"],
+                        },
+                        close: {
+                            rotate: ["45deg", "0deg", "0deg"],
+                            top: ["50%", "50%", "35%"],
+                        },
+                    }}
+                />
+                <motion.span
+                    style={{
+                        left: "50%",
+                        top: "50%",
+                        x: "-50%",
+                        y: "-50%",
+                    }}
+                    variants={{
+                        open: {
+                            rotate: ["0deg", "0deg", "-45deg"],
+                        },
+                        close: {
+                            rotate: ["-45deg", "0deg", "0deg"],
+                        },
+                    }}
+                    className="absolute h-1 w-10 bg-white"
+                />
+                <motion.span
+                    style={{
+                        left: "50%",
+                        bottom: "35%",
+                        x: "-50%",
+                        y: "50%",
+                    }}
+                    variants={{
+                        open: {
+                            bottom: ["35%", "50%", "50%"],
+                            opacity: [1, 1, 0],
+                        },
+                        close: {
+                            bottom: ["50%", "50%", "35%"],
+                            opacity: [0, 1, 1],
+                        },
+                    }}
+                    className="absolute h-1 w-10 bg-white"
+                />
+            </motion.button>
+        </MotionConfig>
+    );
+};
+
+export default HamburgerMenu;
